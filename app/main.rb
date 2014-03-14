6e9b177e5fc7dfbb926da8295f3870b729656a3f
Cara Heacock <caraheacock@gmail.com>
2014-03-13 22:14:58 -0500

    Sign up is working.

--- app/main.rb
+++ app/main.rb
@@ -9,4 +9,28 @@ class Blog < Sinatra::Base
   get "/" do
     erb :home
   end
+  
+  post "/log_in" do
+  end
+  
+  post "/sign_up" do
+    user = User.create({
+      :username => params[:username],
+      :password => params[:password],
+      :first_name => params[:first_name],
+      :last_name => params[:last_name],
+      :email => params[:email]
+    })
+    
+    params[:id] = user.id
+    
+    redirect to("/blog/#{params[:id]}")
+  end
+  
+  get "/blog/:id" do
+    @user = User.find(params[:id])
+    binding.pry
+    
+    "Hi, #{@user.first_name}!"
+  end
 end
